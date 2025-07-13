08de32a09c8db47cfbf5f827fcdf8efc5ccdde2d
rtjil <RTJIL@users.noreply.github.com>
2025-07-13 13:36:21 +0000

    v1.0.4 session fix

--- db/seed.js
+++ db/seed.js
@@ -17,15 +17,6 @@ CREATE TABLE IF NOT EXISTS messages (
    user_id INTEGER,
    FOREIGN KEY (user_id) REFERENCES users (id) 
 );
-
-CREATE TABLE IF NOT EXISTS session (
-  sid varchar NOT NULL COLLATE "default",
-  sess json NOT NULL,
-  expire timestamp(6) NOT NULL,
-  CONSTRAINT session_pkey PRIMARY KEY (sid)
-);
-
-CREATE INDEX IF NOT EXISTS IDX_session_expire ON session(expire);
 `
 try {
   console.log('🔃Creating tables')
